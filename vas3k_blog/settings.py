9520aaa1d4ac28b68296d4d32af53e39917c5118
Nik ILyushkin <nikolay.ilyushkin@me.com>
2024-04-27 18:07:42 +1200

    Update settings.py

--- vas3k_blog/settings.py
+++ vas3k_blog/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "frontend/static",
 ]
 
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
