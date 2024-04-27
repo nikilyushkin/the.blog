a76fa6761c23426a5175394b4f3b8fda5da0f2fb
Nik ILyushkin <nikolay.ilyushkin@me.com>
2024-04-27 18:39:59 +1200

    Update settings.py

--- vas3k_blog/settings.py
+++ vas3k_blog/settings.py
@@ -143,9 +143,9 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend/static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "frontend/static",
+#]
 
 STATIC_ROOT=os.path.join(BASE_DIR,"frontend/static")
 
