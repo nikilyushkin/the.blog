7814c1c105d2846dc5444724c4bcd72f1bd4c1a9
Nik ILyushkin <nikolay.ilyushkin@me.com>
2024-09-05 17:28:09 +1200

    minus serif font

--- vas3k_blog/settings.py
+++ vas3k_blog/settings.py
@@ -184,7 +184,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
-DEFAULT_FROM_EMAIL = "Nik here <hi@nikilyushk.in>"
+DEFAULT_FROM_EMAIL = "Nik is here <hi@nikilyushk.in>"
 
 # Telegram
 
